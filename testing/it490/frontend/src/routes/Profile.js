4a7eafc8e71a70f8ddee191c19bef3459bd343f6
jam374 <134884537+jam374@users.noreply.github.com>
2025-07-21 01:16:40 -0400

    Delete testing/it490/frontend directory

--- testing/it490/frontend/src/routes/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ProfilePage from "../components/ProfilePage.js"
-
-function Profile(){
-    return (
-        <ProfilePage />
-    );
-}
-export default Profile;
--- testing/it490/frontend/src/routes/Registration.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Registration from "../components/Registration.js"
-
-function Register(){
-    return (
-        <Registration />
-    );
-}
-export default Register;
