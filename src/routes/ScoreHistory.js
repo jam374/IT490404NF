71d0a3a26d101a454e5bad764c2ee33d794d22d2
jam374 <134884537+jam374@users.noreply.github.com>
2025-07-23 21:40:00 -0400

    Create ScoreHistory.js

---
+++ src/routes/ScoreHistory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScoreHistory from '../components/ScoreHistory';
+
+const ScoreHistoryRoute = () => {
+  return (
+    <div className="page-container">
+      <ScoreHistory />
+    </div>
+  );
+};
+
+export default ScoreHistoryRoute;
